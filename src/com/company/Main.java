347ad0a7e016cb203c4700b68646dd0c2d87c9ef
JH <https://github.com/jherman-mcc/MAD-105aboutme.git>
2021-05-24 18:14:34 -0500

    Re-share to Github

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// using println
+        System.out.println("Hi! My name is Jeff.");
+        System.out.println("I like to program. I haven't done it in a while though.");
+        System.out.println("I have been ignoring Java since the late 90's.");
+        System.out.println("I heard it has changed a lot in that time.");
+    }
+}
